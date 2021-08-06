1ca13852c8169e033b975ed1343bc4a2d15bc56d
criss <248817095@qq.com>
2021-08-06 13:41:29 +0800

    canvas学习

--- node基础/promise基础.js
+++ node基础/promise基础.js
@@ -1,17 +1,35 @@
-const promise = new Promise((res, rej) => {
-  setTimeout(() => {
-    if (true) {
-      res('成功')
-    }
-    else {
-      rej('失败')
-    }
-  }, 2000)
-})
+// const promise = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     if (true) {
+//       res('成功')
+//     }
+//     else {
+//       rej('失败')
+//     }
+//   }, 2000)
+// })
 
-promise.then(result => {
+// promise.then(result => {
+//   console.log(result);
+// })
+//   .catch(result => {
+//     console.log(result);
+//   })
+
+function promise() {
+  return new Promise(function (res, rej) {
+    setTimeout(function () {
+      if (false) {
+        res('成功')
+      } else {
+        rej('失败')
+      }
+    }, 1000)
+  })
+}
+
+promise().then(function (result) {
   console.log(result);
-})
-.catch(result => {
-  console.log(result);
+}).catch(function (err) {
+  console.log(err);
 })
